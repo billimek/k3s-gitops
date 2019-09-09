7e328866b269f38c4e436761b8cb2e1b00856d74
Jeff Billimek <jeff@billimek.com>
2019-09-09 16:14:47 -0400

    re-writing bootstrap and teardown scripts

--- setup/README.md
+++ setup/README.md
@@ -2,83 +2,34 @@
 
 ## k3s node installation
 
-**TODO:** automate this more, perhaps as part of terraform
-
-### master node
-
-on the k3s 'master' k3s node, we disable servicelb and traefik because we're deploying metallb and traefik ourselves:
-
 ```shell
-k3sup install --ip 10.2.0.30 --k3s-extra-args '--no-deploy servicelb --no-deploy traefik' --user ubuntu
+./bootstrap-k3s.sh
 ```
 
-### worker nodes
+This [script](bootstrap-k3s.sh) does several things:
 
-#### amd64
+1. Installs k3s to the master node
+1. Installs k3s workers to other nodes (mixed amd64 and arm architecture)
+1. Retrieves the new kubeconfig file and places it in $REPO_ROOT/setup/kubeconfig
+1. Installs helm
+1. Installs flux
+1. Retrieves the new flux public key and saves it to the GitHub repo as a repo key (see [add-repo-key.sh](add-repo-key.sh))
+1. Bootstraps the vault-secret-operator with the auto-unwrap token
+1. Bootstraps cert-manager with letsencrypt information
+1. Bootstraps vault (see [bootstrap-vault.sh](bootstrap-vault.sh) for more detail)
+   * Initializes vault if it has not already been initialized
+   * Unseals vault
+   * Configures vault to accept requests from vault-secrets-operator
+   * Writes all secrets (held locally in the `.env` file) to vault for vault-secrets-operator to act on
 
-```shell
-for node in $(echo "10.2.0.31 10.2.0.32"); do
-  k3sup join --ip "$node" --server-ip 10.2.0.30 --user ubuntu
-done
-```
-
-#### arm (e.g. rpi4)
-
-We add a node taint to prevent scheduling unless there is a toleration in place. See [this comment](https://github.com/billimek/homelab-infrastructure/issues/2#issuecomment-522558754) for some background.
-
-```shell
-for node in $(echo "pi4-a pi4-b"); do
-  ssh pi@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://10.2.0.30:6443 K3S_TOKEN=$(ssh ubuntu@10.2.0.30 'sudo cat /var/lib/rancher/k3s/server/node-token') sh -s - --node-taint arm=true:NoExecute"
-done
-```
-
-## k3s teardown (uninstall)
+## k3s teardown (uninstall everything)
 
 ```shell
-for node in $(echo "pi4-b pi4-a"); do
-  ssh pi@"$node" "k3s-agent-uninstall.sh"
-done
-for node in $(echo "10.2.0.32 10.2.0.31"); do
-  ssh ubuntu@"$node" "k3s-agent-uninstall.sh"
-done
-ssh ubuntu@10.2.0.30 "/usr/local/bin/k3s-uninstall.sh"
+./teardown-k3s.sh
 ```
 
-## bootstrapping k3s cluster
+This [script](teardown-k3s.sh) will:
 
-**TODO:** automate this more, perhaps as part of terraform
-
-### helm
-
-```shell
-kubectl -n kube-system create sa tiller
-kubectl create clusterrolebinding tiller-cluster-rule \
-    --clusterrole=cluster-admin \
-    --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-```
-
-### flux
-
-* Install flux.  Where `git.url` should define the repo where the GitOps code lives:
-
-```shell
-helm repo add fluxcd https://charts.fluxcd.io
-helm upgrade --install flux --values flux-values.yaml --namespace flux fluxcd/flux
-```
-
-* Once flux is installed, [get the SSH key and give it write access to the github repo](https://docs.fluxcd.io/en/latest/tutorials/get-started-helm.html#giving-write-access):
-
-```shell
-kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
-```
-
-* Add the key to the repo as a deploy key with write access as [described in the instructions](https://docs.fluxcd.io/en/latest/tutorials/get-started-helm.html#giving-write-access)
-
-### init_script.sh
-
-This script creates necessary manual yaml insertions and vault secret population.  See [init_script.sh](init_script.sh) for more details.
-
-```shell
-init_script.sh
-```
+1. Remove all pods and pvcs
+1. Uninstall k3s from all worker nodes
+1. Uninstall k3s from the master node
---
+++ setup/bootstrap-k3s.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# nodes
+K3S_MASTER="k3s-0"
+K3S_WORKERS_AMD64="k3s-1 k3s-2"
+K3S_WORKERS_RPI="pi4-a pi4-b pi4-c"
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+need() {
+    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
+}
+
+need "curl"
+need "ssh"
+need "kubectl"
+need "helm"
+
+k3sMasterNode() {
+  echo "installing k3s master to $K3S_MASTER"
+  ssh ubuntu@"$K3S_MASTER" "curl -sLS https://get.k3s.io | INSTALL_K3S_EXEC='server --tls-san $K3S_MASTER --no-deploy servicelb --no-deploy traefik' sh -"
+  ssh ubuntu@"$K3S_MASTER" "sudo cat /etc/rancher/k3s/k3s.yaml | sed 's/server: https:\/\/localhost:6443/server: https:\/\/$K3S_MASTER:6443/'" > "$REPO_ROOT/setup/kubeconfig"
+  NODE_TOKEN=$(ssh ubuntu@"$K3S_MASTER" "sudo cat /var/lib/rancher/k3s/server/node-token")
+}
+
+ks3amd64WorkerNodes() {
+  for node in $K3S_WORKERS_AMD64; do
+    echo "joining amd64 $node to $K3S_MASTER"
+    ssh ubuntu@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-0:6443 K3S_TOKEN=$NODE_TOKEN sh -s -"
+  done
+}
+
+ks3armWorkerNodes() {
+  for node in $K3S_WORKERS_RPI; do
+    echo "joining pi4 $node to $K3S_MASTER"
+    ssh pi@"$node" "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-0:6443 K3S_TOKEN=$NODE_TOKEN sh -s - --node-taint arm=true:NoExecute --data-dir /mnt/usb/var/lib/rancher"
+  done
+}
+
+installHelm() {
+  # generate files locally instead of against the cluster directly ?
+  # TMPFILE=`mktemp /tmp/helm.XXXXXX` || exit 1
+  # kubectl -n kube-system create sa tiller --dry-run -o=yaml >> "$TMPFILE"
+  # echo "---" >> "$TMPFILE"
+  # kubectl create clusterrolebinding tiller-cluster-rule \
+  #     --clusterrole=cluster-admin \
+  #     --serviceaccount=kube-system:tiller \
+  #     --dry-run -o=yaml >> "$TMPFILE"
+  # echo "---" >> "$TMPFILE"
+  # helm init --service-account tiller -o=yaml >> "$TMPFILE"
+
+  kubectl -n kube-system create sa tiller
+  kubectl create clusterrolebinding tiller-cluster-rule \
+      --clusterrole=cluster-admin \
+      --serviceaccount=kube-system:tiller
+  helm init --wait --service-account tiller
+
+  HELM_SUCCESS="$?"
+  if [ "$HELM_SUCCESS" != 0 ]; then
+    echo "helm init failed - no bueno!"
+    exit 1
+  fi
+}
+
+installFlux() {
+  # install flux
+  helm repo add fluxcd https://charts.fluxcd.io
+  helm upgrade --install flux --values "$REPO_ROOT"/setup/flux-values.yaml --namespace flux fluxcd/flux
+
+  FLUX_READY=1
+  while [ $FLUX_READY != 0 ]; do
+    echo "waiting for flux account to be fully ready..."
+    kubectl -n flux wait --for condition=available deployment/flux
+    FLUX_READY="$?"
+    sleep 5
+  done
+
+  # grab output the key
+  FLUX_KEY=$(kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2)
+
+  echo "adding the key to github automatically"
+  "$REPO_ROOT"/setup/add-repo-key.sh "$FLUX_KEY"
+}
+
+kapply() {
+  if output=$(envsubst < "$@"); then
+    printf '%s' "$output" | kubectl apply -f -
+  fi
+}
+
+installManualObjects(){
+  . "$REPO_ROOT"/setup/.env
+
+  ##########
+  # secrets
+  ##########
+  kubectl --namespace kube-system delete secret vault
+  kubectl --namespace kube-system create secret generic vault --from-literal=vault-unwrap-token="$VAULT_UNSEAL_TOKEN"
+
+  #########################
+  # cert-manager bootstrap
+  #########################
+  CERT_MANAGER_READY=1
+  while [ $CERT_MANAGER_READY != 0 ]; do
+    echo "waiting for cert-manager to be fully ready..."
+    kubectl -n kube-system wait --for condition=Available deployment/cert-manager > /dev/null 2>&1
+    CERT_MANAGER_READY="$?"
+    sleep 5
+  done
+  kapply "$REPO_ROOT"/kube-system/cert-manager/cert-manager-letsencrypt.txt
+}
+
+k3sMasterNode
+ks3amd64WorkerNodes
+ks3armWorkerNodes
+
+export KUBECONFIG="$REPO_ROOT/setup/kubeconfig"
+installHelm
+installFlux
+installManualObjects
+
+# bootstrap vault
+"$REPO_ROOT"/setup/bootstrap-vault.sh
