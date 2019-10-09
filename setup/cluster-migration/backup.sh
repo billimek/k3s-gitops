9042087880588579168d78e85d94881b40444ae9
Jeff Billimek <jeff@billimek.com>
2019-10-09 16:52:19 -0400

    refactor backup script to handle rbd dest

--- setup/cluster-migration/backup.sh
+++ setup/cluster-migration/backup.sh
@@ -9,8 +9,8 @@
 #   4. (HIGHLY RECCOMENDED) source and destination workloads are scaled-to-zero prior to running this
 
 PVCS_TO_BACKUP="home-assistant mc-minecraft-datadir mcsv-minecraft-datadir node-red kube-plex-config radarr-config rtorrent-flood-config sonarr-config unifi influxdb prometheus-operator-grafana"
-
-PVCS_TO_RESTORE="home-assistant mc-minecraft-datadir mcsv-minecraft-datadir kube-plex-config radarr-config rtorrent-flood-config sonarr-config unifi prometheus-operator-grafana"
+PVCS_TO_RESTORE_CEPHFS=""
+PVCS_TO_RESTORE_RBD="home-assistant mc-minecraft-datadir mcsv-minecraft-datadir kube-plex-config radarr-config rtorrent-flood-config sonarr-config unifi prometheus-operator-grafana"
 PVCS_TO_RESTORE_NFS="node-red influxdb"
 
 #### backup the stuff from (external) ceph rbd
@@ -26,12 +26,12 @@ done
 
 #### restore the stuff to cephfs
 export KUBECONFIG=/home/jeff/src/k3s-gitops/setup/kubeconfig
-for pvc in $PVCS_TO_RESTORE
+for pvc in $PVCS_TO_RESTORE_CEPHFS
 do
   PV=$(kubectl get pv --all-namespaces | grep "$pvc" | awk '{print $1}')
-  CEPHFS_THING=$(kubectl describe pv "$PV" | grep VolumeHandle | awk '{print $2}' | sed 's/[0-9]*-[0-9]*-rook-ceph-[0-9]*-\(.*\)/\1/g')
-  TOOLBOX_PATH="/tmp/registry/volumes/csi/csi-vol-$CEPHFS_THING"
-  echo "===== Restoring $pvc ($CEPHFS_THING) from proxmox:/tank/backups/cluster/${pvc}.tar.gz"
+  CEPH_CSI_THING=$(kubectl describe pv "$PV" | grep VolumeHandle | awk '{print $2}' | sed 's/[0-9]*-[0-9]*-rook-ceph-[0-9]*-\(.*\)/\1/g')
+  TOOLBOX_PATH="/tmp/registry/volumes/csi/csi-vol-$CEPH_CSI_THING"
+  echo "===== Restoring $pvc ($CEPH_CSI_THING) from proxmox:/tank/backups/cluster/${pvc}.tar.gz"
   tools="$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')"
   echo "     ----- deleting contents of $TOOLBOX_PATH in the toolbox pod"
   kubectl -n rook-ceph exec -it "$tools" -- sh -c "rm -rf $TOOLBOX_PATH/{*,.*} 2> /dev/null"
@@ -42,11 +42,24 @@ do
   kubectl -n rook-ceph exec -it "$tools" -- sh -c "rm /tmp/${pvc}.tar.gz"
 done
 
+#### restore the stuff to ceph-block (rbd)
+for pvc in $PVCS_TO_RESTORE_RBD
+do
+  PV=$(kubectl get pv --all-namespaces | grep "$pvc" | awk '{print $1}')
+  CEPH_CSI_THING=$(kubectl describe pv "$PV" | grep VolumeHandle | awk '{print $2}' | sed 's/[0-9]*-[0-9]*-rook-ceph-[0-9]*-\(.*\)/\1/g')
+  echo "===== Restoring $pvc ($CEPH_CSI_THING) from proxmox:/tank/backups/cluster/${pvc}.tar.gz"
+  tools="$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')"
+  echo "     ----- copying /mnt/backups/cluster/${pvc}.tar.gz to $tools:/tmp/"
+  kubectl -n rook-ceph cp /mnt/backups/cluster/"${pvc}.tar.gz" "$tools":/tmp/
+  echo "     ----- mapping, mounting, deleting contents of $pvc from $CEPH_CSI_THING and untarring /tmp/${pvc}.tar.gz to /tmp/rbd/ in the toolbox pod"
+  kubectl -n rook-ceph exec -it "$tools" -- sh -c "DEV=\$(rbd map replicapool/csi-vol-$CEPH_CSI_THING) && mkdir -p /tmp/rbd && mount \$DEV /tmp/rbd && cd /tmp/rbd && tar zxf /tmp/${pvc}.tar.gz && cd /tmp; umount /tmp/rbd && rbd unmap \$DEV && rm /tmp/${pvc}.tar.gz"
+done
+
 #### restore the stuff to nfs-client
 for pvc in $PVCS_TO_RESTORE_NFS
 do
   PV=$(kubectl get pv --all-namespaces | grep "$pvc" | awk '{print $1}')
   NFS_PATH=$(kubectl get pv "$PV" -o=jsonpath='{.spec.nfs.path}')
   echo "===== Restoring $pvc ($NFS_PATH) from proxmox:/tank/backups/cluster/$pvc"
-  ssh root@proxmox "rm -rf $NFS_PATH/* ; rm -rf $NFS_PATH/.* ; cp -a /tank/backups/cluster/$pvc/* $NFS_PATH/ ; cp -a /tank/backups/cluster/$pvc/.* $NFS_PATH/"
+  ssh root@proxmox sh -c "rm -rf $NFS_PATH/* 2> /dev/null; rm -rf $NFS_PATH/.* 2> /dev/null; cd $NFS_PATH; tar zxf /tank/backups/cluster/${pvc}.tar.gz"
 done
